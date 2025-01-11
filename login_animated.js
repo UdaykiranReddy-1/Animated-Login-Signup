10f2c137f82ba5d5337b511b5849eeb034135db9
UdaykiranReddy-1 <118445671+UdaykiranReddy-1@users.noreply.github.com>
2025-01-11 16:13:36 +0530

    Update login_animated.js

--- login_animated.js
+++ login_animated.js
@@ -1,9 +1,32 @@
 const login = document.querySelector('.loginsection');
 const loginlink = document.querySelector('.loginlink');
 const registerlink = document.querySelector('.registerlink');
+
 registerlink.addEventListener('click',()=>{
     login.classList.add('active');
 })
 loginlink.addEventListener('click',()=>{
     login.classList.remove('active');
 })
+
+document.querySelectorAll('.inputbox input').forEach(input => {
+    input.addEventListener('input', () => {
+        if (input.value.trim() !== '') {
+            input.classList.add('has-content');
+        } else {
+            input.classList.remove('has-content');
+        }
+    });
+});
+
+function togglePassword(fieldId, element) {
+    const input = document.getElementById(fieldId);
+    const icon = element.querySelector("i");
+    if (input.type === "password") {
+        input.type = "text";
+        icon.classList.replace("bxs-show", "bxs-hide");
+    } else {
+        input.type = "password";
+        icon.classList.replace("bxs-hide", "bxs-show");
+    }
+}
